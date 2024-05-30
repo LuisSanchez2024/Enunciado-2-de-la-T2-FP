bb947ec5851b87555efcfa7bad900f5039ac6a49
LuisSanchez2024 <n00394775@upn.pe>
2024-05-30 17:56:53 -0500

    Update Program.cs

--- Enunciado 2 de la T2/Program.cs
+++ Enunciado 2 de la T2/Program.cs
@@ -11,69 +11,69 @@ namespace Enunciado_2_de_la_T2
         static void Main(string[] args)
         {
 
-            List<int> numeros = new List<int>();
+            List<int> números = new List<int>();
 
             // Insertar números aleatorios en la lista
             Random rand = new Random();
             for (int i = 0; i < 10; i++)
             {
-                int numeroAleatorio = rand.Next(1, 101); // Genera números entre 1 y 100
-                numeros.Add(numeroAleatorio);
+                int numero_Aleatorio = rand.Next(1, 101); // números entre 1 y 100
+                números.Add(numero_Aleatorio);
             }
 
             Console.WriteLine("Números insertados en la lista:");
-            foreach (var numero in numeros)
+            foreach (var número in números)
             {
-                Console.Write(numero + " ");
+                Console.Write(número + " ");
             }
             Console.WriteLine();
 
             // Ordenar de forma ascendente
-            numeros.Sort();
-            Console.WriteLine("Números ordenados de forma ascendente:");
-            foreach (var numero in numeros)
+            números.Sort();
+            Console.WriteLine("Números ordenados de manera ascendente:");
+            foreach (var número in números)
             {
-                Console.Write(numero + " ");
+                Console.Write(número + " ");
             }
             Console.WriteLine();
 
             // Ordenar de forma descendente
-            numeros.Reverse();
-            Console.WriteLine("Números ordenados de forma descendente:");
-            foreach (var numero in numeros)
+            números.Reverse();
+            Console.WriteLine("Números ordenados de manera descendente:");
+            foreach (var número in números)
             {
-                Console.Write(numero + " ");
+                Console.Write(número + " ");
             }
             Console.WriteLine();
 
             // Buscar un elemento en la lista
             Console.Write("Ingresa un número para buscar en la lista: ");
-            int numeroBuscado = Convert.ToInt32(Console.ReadLine());
-            if (numeros.Contains(numeroBuscado))
+            int numero_Buscado = Convert.ToInt32(Console.ReadLine());
+            if (números.Contains(numero_Buscado))
             {
-                Console.WriteLine($"El número {numeroBuscado} está en la lista.");
+                Console.WriteLine($"El número {numero_Buscado} está en la lista.");
             }
             else
             {
-                Console.WriteLine($"El número {numeroBuscado} no está en la lista.");
+                Console.WriteLine($"El número {numero_Buscado} no está en la lista.");
             }
 
             // Eliminar un número y mostrar la lista actualizada
             Console.Write("Ingresa un número para eliminar de la lista: ");
-            int numeroEliminar = Convert.ToInt32(Console.ReadLine());
-            if (numeros.Remove(numeroEliminar))
+            int numero_Eliminar = Convert.ToInt32(Console.ReadLine());
+            if (números.Remove(numero_Eliminar))
             {
-                Console.WriteLine($"El número {numeroEliminar} se eliminó de la lista.");
+                Console.WriteLine($"El número {numero_Eliminar} se eliminó de la lista.");
             }
             else
             {
-                Console.WriteLine($"El número {numeroEliminar} no estaba en la lista.");
+                Console.WriteLine($"El número {numero_Eliminar} no estaba en la lista.");
             }
 
             Console.WriteLine("Lista actualizada:");
-            foreach (var numero in numeros)
+            foreach (var número in números)
             {
-                Console.Write(numero + " ");
+                Console.Write(número + " ");
             }
             Console.WriteLine();
         }
